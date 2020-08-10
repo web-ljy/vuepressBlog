b238603d08a1653c752a9c6a977dd1a1a853ba99
ljy <767234966@qq.com>
2020-08-10 12:02:40 +0800

    提交文件

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.yourwebsite.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git config user.name "web-ljy"
+git config user.email "767234966@qq.com"
+
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:web-ljy/web-ljy.github.io.git master
+
+# 如果发布到 https://USERNAME.github.io/<REPO>  REPO=github上的项目
+#git push -f git@github.com:web-ljy/web-ljy.github.io.git master:gh-pages
+
+cd -
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,82 @@
+module.exports = {
+  title: 'web-ljy blog',
+  description: '个人博客',
+  head: [                // 注入到当前页面的 HTML <head> 中的标签
+    ['link', { rel: 'icon', href: '/images/logo.jpg' }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  base: '/',            // 这是部署到github相关的配置
+  markdown: {
+    lineNumbers: false  // 代码块显示行号
+  },
+  themeConfig: {
+    nav: [             // 导航栏配置
+
+      { text: 'Home', link: '/guide/JavaScript/' },
+      {
+        text: 'About', items: [      // 当提供了一个items数组而不是一个单一的link时，它将显示为一个下拉列表
+          { text: 'Chinese', link: '/guide/VUE/postC-1/' },
+          { text: 'Japanese', link: '/guide/VUE/postC-2/' }
+        ]
+      },
+    ],
+    sidebar: {         // 侧边栏配置
+      '/guide/': [
+        {
+          title: 'JavaScript', 
+          collapsable: false,  //是否展开，可折叠
+          children: [
+            { title: "深拷贝", path: '/guide/JavaScript/postA-1/' },  // 你的md文件地址（可以省略.md 拓展名，同时以 / 结尾的路径将会被视为 */README.md）
+            { title: "数组去重", path: '/guide/JavaScript/postA-2/' },
+          ]
+        },
+        {
+          title: 'CSS',
+          collapsable: true, 
+          children: [
+            { title: "margin 负值", path: '/guide/CSS/postB-1/' },
+            { title: "三角形", path: '/guide/CSS/postB-2/' },
+          ]
+        },
+        {
+          title: 'VUE', 
+          collapsable: true, 
+          children: [
+            { title: "插槽", path: '/guide/VUE/postC-1/' },
+            { title: "组件", path: '/guide/VUE/postC-2/' },
+          ]
+        },
+      ]
+    }, 
+    footer: {
+      // gitbutton  配置
+      gitbtn: {
+        // 仓库地址
+        repository: "https://ghbtns.com/github-btn.html?user=youdeliang&repo=vuepress-theme-top&type=star&count=true",
+        frameborder: 0,
+        scrolling: 0,
+        width: "80px",
+        height: "20px"
+      },
+      // 添加自定义 footer
+      custom: `Copyright &copy; Top Blog 2020 <br /> 
+        Theme By <a href="VuePress 中文文档 | VuePress 中文网" target="_blank">VuePress</a>
+        | <a href="youdeliang - Overview" target="_blank">youdeliang</a>`,
+      // spin: true,
+      // theme: 'filled'
+    },
+    // 分页配置
+    pagination: {
+      // 每页文章数量
+      perPage: 5,
+    },
+    sidebarDepth: 2,                 // 侧边栏显示2级
+    lastUpdated: '上次更新时间',      // 由于 lastUpdated 是基于 git 的, 所以你只能在一个基于 git 的项目中启用它。
+    search: true,                    // 禁用或者开启内置搜索框，默认为true  
+    searchMaxSuggestions: 10         // 默认是5    
+  },
+  plugins: {
+    '@vuepress/search': true,
+    '@vuepress/last-updated': true, // 文章的末尾会自动显示文章的更新日期
+    '@vuepress/back-to-top': true,  // 回到顶部
+  }
+};
